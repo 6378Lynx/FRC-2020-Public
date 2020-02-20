e73f047776f2a1d031cd9c185f8d5d8ed9091e0b
6378Lynx <44904031+6378Lynx@users.noreply.github.com>
2020-02-20 16:09:36 -0500

    Reads pose value from networktable
    Return functions for offsets.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -13,22 +13,42 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
-  private NetworkTableEntry pose;
-  private NetworkTableEntry isDriverMode;;
+  private double[] pose;
+  private NetworkTableEntry isDriverMode;
+  double[] defaultValue = new double[0];
   private NetworkTableInstance table;
   private NetworkTable cameraTable;
   private String cameraName = "";
+  double curr_X_Distance;
+  double curr_Y_Distance;
+  double curr_angle_offset;
   
 
   public VisionSubsystem() {
     table = NetworkTableInstance.getDefault();
-    cameraTable = table.getTable("chameleon-vision").getSubTable(cameraName);
-    pose = cameraTable.getEntry("targetPose");
+    cameraTable = table.getTable("chameleon-vision").getSubTable(defaultValue);
+    pose = table.getEntry("targetPose").getDoubleArray(pose);
     isDriverMode = cameraTable.getEntry("driver_mode");
   }
 
   @Override
   public void periodic() {
-    //System.out.println(yaw.getDouble(0.0));
+    curr_X_Distance = pose[0];
+    curr_Y_Distance = pose[1];
+    curr_angle_offset = pose[2];
   }
+
+
+  public double x_offset(){
+    return curr_X_Distance;
+  }
+
+  public double y_offset(){
+    return curr_Y_Distance;
+  }
+
+  public double angle_offset(){
+    return curr_angle_offset;
+  }
+
 }
